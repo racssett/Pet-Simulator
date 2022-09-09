7ac2bf9279da05383a0ed05b87e46ef4b385b600
racssett <rachel.c.hassett@gmail.com>
2022-09-09 14:58:46 -0500

    Add Cached Element References, declare varialbes

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*-----------------------Constants-----------------------*/
+
+
+
+
+/*-------------------Variables (state)-------------------*/
+
+let level, timer
+
+/*---------------Cached Element References---------------*/
+
+const messageEl = document.getElementById('message')
+console.log(messageEl)
+
+const icons = document.querySelectorAll('.icons > div')
+console.log(icons)
+
+/*--------------------Event Listeners--------------------*/
+
+
+
+
+/*-----------------------Functions-----------------------*/
+
+// function init () {
+//     timer = 0
+// }
