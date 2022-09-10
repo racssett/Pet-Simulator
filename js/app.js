a6d0c56a79205597b27f4095fb7f47daefa83087
racssett <rachel.c.hassett@gmail.com>
2022-09-10 11:32:28 -0500

    Create rough init function, add more cached element references

--- js/app.js
+++ js/app.js
@@ -15,6 +15,21 @@ console.log(messageEl)
 const icons = document.querySelectorAll('.icons > div')
 console.log(icons)
 
+const firstEvolution = document.getElementById('evolution1')
+console.log(firstEvolution)
+
+const secondEvolution = document.getElementById('evolution2')
+console.log(secondEvolution)
+
+const finalEvolution = document.getElementById('evolution3')
+console.log(finalEvolution)
+
+const startBtn = document.getElementById('start')
+console.log(startBtn)
+
+const restartBtn = document.getElementById('restart')
+console.log(restartBtn);
+
 /*--------------------Event Listeners--------------------*/
 
 
@@ -22,6 +37,12 @@ console.log(icons)
 
 /*-----------------------Functions-----------------------*/
 
-// function init () {
-//     timer = 0
-// }
+function init () {
+    timer = 0
+    level = 1
+    firstEvolution.style = "display: block"
+    secondEvolution.style = "display: hidden"
+    finalEvolution.style = "display: hidden"
+    startBtn.style = "display: block"
+    restartBtn.style = "display: hidden"
+}
